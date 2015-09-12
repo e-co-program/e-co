6b08186d1f920217d78524fc048ee1d9aa038a5d
Tomofumi <widevideo.510@gmail.com>
2015-09-12 14:12:07 +0900

    test.js削除

--- e_co/src/main/resources/static/js/search/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-
-function searchTest() {
-	$('.menu').append("<li class='event'><a class='menuitem submenuheader' >テスト<span>　2014/4/4</span></a><div class='submenu'><ul class='menu2'><li><a href='search_result.html' target='main'>テスト01</a></li><li><a href='search_result.html' target='main'テスト02</a></li></ul></div></li>");
-};
-
-searchTest();
